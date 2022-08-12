41dec3bc45416c0549b067158457555c7f0a1024
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2022-08-12 01:09:11 -0700

    added and applied function for stat button

--- sample1/script.js
+++ sample1/script.js
@@ -1,5 +1,6 @@
+//Info button function
 var infoButton = document.getElementById('infoButton');
-var info = document.querySelector(".info") 
+var info = document.querySelector(".info")
 
 function displayInfo() {
     if (info.style.display === "none") {
@@ -8,3 +9,18 @@ function displayInfo() {
         info.style.display = "none";
     }
 }
+
+//Stat button function
+var statButton = document.getElementById('statButton');
+var stats = document.querySelector(".stats")
+
+function displayStats() {
+    if (stats.style.display === "none") {
+        stats.style.display = "block";
+    } else {
+        stats.style.display = "none";
+    }
+}
+
+
+
