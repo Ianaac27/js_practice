9ccbfc6b2bea0c832851f5dc19b994a6c343f880
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2022-08-12 01:22:04 -0700

    updated button functions to only show 1 set of information at a time, if other button is selected while current info is shown it will toggle to new info

--- sample1/script.js
+++ sample1/script.js
@@ -1,26 +1,26 @@
-//Info button function
+//Info button variables
 var infoButton = document.getElementById('infoButton');
 var info = document.querySelector(".info")
 
+//Stat button variables
+var statButton = document.getElementById('statButton');
+var stats = document.querySelector(".stats")
+
+//Button functions
 function displayInfo() {
     if (info.style.display === "none") {
         info.style.display = "block";
+        stats.style.display = "none";
     } else {
         info.style.display = "none";
     }
 }
-
-//Stat button function
-var statButton = document.getElementById('statButton');
-var stats = document.querySelector(".stats")
-
 function displayStats() {
     if (stats.style.display === "none") {
         stats.style.display = "block";
+        info.style.display = "none";
     } else {
         stats.style.display = "none";
     }
 }
-
-
 
